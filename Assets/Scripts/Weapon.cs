5d559a728441f8903bb312bbe0e5ced966b73e69
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-03-09 14:12:12 +0900

    총구화염 파티클시스템 추가
    사격 관통 효과는 파티클시스템이 스타터에셋에 없어서 추가하지 않음

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -8,6 +9,7 @@ public class Weapon : MonoBehaviour
     [SerializeField] Camera FPSCamera;
     [SerializeField] float range = 100f;
     [SerializeField] float damage = 30f;
+    [SerializeField] ParticleSystem muzzleVFX;
 
     void Update()
     {
@@ -19,20 +21,32 @@ public class Weapon : MonoBehaviour
 
     void Shoot()
     {
+        PlayMuzzleFlash();
+        ProcessRaycast();
+
+    }
+
+    void PlayMuzzleFlash()
+    {
+        muzzleVFX.Play();
+        
+
+    }
+
+    void ProcessRaycast()
+    {
         RaycastHit hit;
-        if(Physics.Raycast(FPSCamera.transform.position, FPSCamera.transform.forward, out hit, range))
+        if (Physics.Raycast(FPSCamera.transform.position, FPSCamera.transform.forward, out hit, range))
         {
             Debug.Log(hit.transform.name);
             //ToDo : 
             EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
-            if(target == null)  { return; }
+            if (target == null) { return; }
             target.TakeDamage(damage);
         }
         else
         {
             return;
         }
-
-        
     }
 }
