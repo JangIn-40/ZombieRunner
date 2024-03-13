46f46a097e4cee3a35afb7869f23eaec886f52a4
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-03-13 22:35:11 +0900

    탄약기능 추가

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -10,6 +10,7 @@ public class Weapon : MonoBehaviour
     [SerializeField] float range = 100f;
     [SerializeField] float damage = 30f;
     [SerializeField] ParticleSystem muzzleVFX;
+    [SerializeField] Ammo ammoSlot;
 
     void Update()
     {
@@ -21,28 +22,28 @@ public class Weapon : MonoBehaviour
 
     void Shoot()
     {
-        PlayMuzzleFlash();
-        ProcessRaycast();
-
+        if(ammoSlot.GetCurrentAmmo() > 0)
+            {
+                PlayMuzzleFlash();
+                ProcessRaycast();
+                ammoSlot.ReduceCurrentAmmo();
+            }
     }
 
     void PlayMuzzleFlash()
     {
         muzzleVFX.Play();
-        
-
     }
 
     void ProcessRaycast()
     {
         RaycastHit hit;
         if (Physics.Raycast(FPSCamera.transform.position, FPSCamera.transform.forward, out hit, range))
-        {
-            Debug.Log(hit.transform.name);
-            //ToDo : 
-            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
-            if (target == null) { return; }
-            target.TakeDamage(damage);
+        {   
+                Debug.Log(hit.transform.name);
+                EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+                if (target == null) { return; }
+                target.TakeDamage(damage);      
         }
         else
         {
