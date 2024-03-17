e97edd3a4098ad2e153802557b3db8b264b11817
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-03-17 23:01:08 +0900

    무기 휠과 버튼교체 추가

--- Assets/Scripts/WeaponSwitcher.cs
+++ Assets/Scripts/WeaponSwitcher.cs
@@ -13,6 +13,61 @@ public class WeaponSwitcher : MonoBehaviour
         SetWeaponActive();
     }
 
+    void Update()
+    {
+        int previousWeapon = currentWeapon;
+
+        ProcessKeyInput();
+        ProcessScrollWheel();
+
+        if(previousWeapon != currentWeapon)
+        {
+            SetWeaponActive();
+        }
+    }
+
+    void ProcessKeyInput()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            currentWeapon = 0;
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            currentWeapon = 1;
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            currentWeapon = 2;
+        }
+    }
+
+    private void ProcessScrollWheel()
+    {
+        if(Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            if(currentWeapon >= transform.childCount - 1)
+            {
+                currentWeapon = 0;
+            }
+            else
+            {
+                currentWeapon++;
+            }
+        }
+        else if(Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            if(currentWeapon <= 0)
+            {
+                currentWeapon = transform.childCount - 1;
+            }
+            else
+            {
+                currentWeapon--;
+            }
+        }
+    }
+
     void SetWeaponActive()
     {
         int weaponIndex = 0;
@@ -30,9 +85,6 @@ public class WeaponSwitcher : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
+
 }
